b9048165e95534ba70baf4931a3cda074284f469
Tyler Margison <kolorahl@gmail.com>
2017-01-19 21:48:41 -0800

    Convert BertWS to ES6

--- src/bertws.js
+++ src/bertws.js
@@ -1,78 +1,80 @@
-var BertWS = function(url, handlers) {
-    ws = new WebSocket(url);
+import Bert from "./bert"
 
-    // We want to parse arrays, not blobs.
-    ws.binaryType = "arraybuffer";
+class BertWS {
+  constructor(url, handlers={}) {
+    this.ws = new WebSocket(url)
+    this.ws.binaryType = "arraybuffer"
 
-    // Assign handles, using a basic default handler when one is missing.
-    if (handlers.ws_open != null) { ws.onopen = handlers.ws_open; }
+    if (handlers.open != null) { this.ws.onopen = handlers.open }
     else {
-        ws.onopen = function(e) {
-            "WebSocket connection to " + ws.url + " opened.";
-        }
-    }
-    if (handlers.ws_close != null) { ws.onclose = handlers.ws_close; }
-    else {
-        ws.onclose = function(e) {
-            "WebSocket connection to " + ws.url + " closed (" + e.code + ")";
-        }
-    }
-    if (handlers.ws_error != null) { ws.onerror = handlers.ws_error; }
-    else {
-        ws.onerror = function(e) {
-            "WebSocket connection to " + ws.url + " has thrown an error.";
-        }
+      this.ws.onopen = (e) => {
+        console.log("WebSocket connection to " + url + " opened.", e)
+      }
     }
 
-    // Ideally we should have checked that these exist, as these handlers are
-    // required, but this is just a "demo" file so we aren't going to.
-    ws.handle_success = handlers.success;
-    ws.handle_error = handlers.error;
+    if (handlers.close != null) { this.ws.onclose = handlers.close }
+    else {
+      this.ws.onclose = (e) => {
+        console.log("WebSocket connection to " + url + " closed.", e)
+      }
+    }
+
+    if (handlers.error != null) { this.ws.onerror = handlers.error }
+    else {
+      this.ws.onerror = (e) => {
+        console.log("WebSocket connection to " + url + " has thrown an error.", e)
+      }
+    }
+
+    if (handlers.success != null) {this.on_success = handlers.success }
+    else {
+      this.on_success = (e) => {
+        console.log("WebSocket connection to " + url + " received data.", e)
+      }
+    }
+
+    if (handlers.error != null) {this.on_error = handlers.error }
+    else {
+      this.on_error = (e) => {
+        console.log("WebSocket connection to " + url + " received an error.", e)
+      }
+    }
 
     // When we receive message from the WebSocket...
-    ws.onmessage = function(e) {
-        // Create a "byte array" of the data that we can parse.
-        var dv = new DataView(e.data);
-        var arr = new Uint8Array(dv.byteLength);
-        for (var i = dv.byteOffset; i < arr.length; i++) {
-            arr[i] = dv.getUint8(i);
-        }
-        // Use Bert to decode the byte array.
-        var message = Bert.decode(arr);
-        // We check for errors by looking for the common tuple: {:error, term}
-        if (message instanceof Array && message[0] == "error") {
-            ws.handle_error(message);
-        }
-        else {
-            ws.handle_success(message);
-        }
-    };
+    this.ws.onmessage = this.receive.bind(this)
+  }
 
-    // When we want to send a Bert object over the WebSocket...
-    ws.send_bert = function(bertObj, success, error) {
-        // Create a "byte array" of the data that we can send.
-        var data = new Uint8Array(Bert.encode(bertObj));
+  receive(e) {
+    let message = this.decode(e.data)
 
-        // Individual functions can temporarily override the handle given during
-        // WebSocket creation.
-        var original_success = ws.handle_success;
-        if (success != null) {
-            ws.handle_success = function(message) {
-                ws.handle_success = original_success;
-                success(message);
-            };
-        }
-        var original_error = ws.handle_error;
-        if (error != null) {
-            ws.handle_error = function(message) {
-                ws.handle_error = original_error;
-                error(message);
-            };
-        }
+    // We check for errors by looking for the common tuple: {:error, term}
+    if (message instanceof Array && message[0] == "error")
+      this.on_error(message)
+    else
+      this.on_success(message)
+  }
 
-        // Beam me up, Scotty!
-        ws.send(data);
-    };
+  decode(data) {
+    // Create a "byte array" of the data that we can parse.
+    let dview = new DataView(data),
+        bytes = new Uint8Array(dview.byteLength)
+    for (var i = dview.byteOffset; i < bytes.length; i++) {
+      bytes[i] = dview.getUint8(i)
+    }
 
-    return ws;
-};
+    // Use Bert to decode the byte array.
+    return Bert.decode(bytes)
+  }
+
+  send(message) {
+    // Create a "byte array" of the data that we can send.
+    let data = this.encode(message)
+    this.ws.send(data)
+  }
+
+  encode(data) {
+    return new Uint8Array(Bert.encode(data))
+  }
+}
+
+export default BertWS
