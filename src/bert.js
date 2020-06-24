89a1dda66e1c1a67180f02735fb579062e24d05f
Tyler Eon <kolorahl@gmail.com>
2020-06-23 19:23:58 -0700

    Change to the GNU GPL v3 software license

--- src/bert.js
+++ src/bert.js
@@ -1,3 +1,19 @@
+// bertjs - Encode and Decode BERT data
+// Copyright (C) 2015-2020  Tyler Eon
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // Most of these tokens map to a function for encoding/decoding.
 // This is subset of allowed tokens in the Erlang external term format.
 // Yup. Magic numbers.
